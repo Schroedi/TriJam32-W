63cb8a2adf8ed0c35622871de8d6a0f28caa3349
Christoph Schroeder <call-schroedi@web.de>
2019-08-17 22:45:35 +0200

    Impulse on split, refator into scenes.

--- Char.gd
+++ Char.gd
@@ -2,6 +2,11 @@ extends RigidBody2D
 
 func split():
 	var clone = duplicate()
+	clone.position.y -= 20
+	position.y += 20
+	clone.apply_impulse(Vector2(0,0), Vector2(0, -200))
+	clone.get_node("Label").text = "-"
+	self.apply_impulse(Vector2(0,0), Vector2(0, 200))
 	get_parent().add_child(clone)
 	State.chars += 1
 
