d4964a72e6b829215a67ea2c85be1785f518ac99
Christoph Schroeder <call-schroedi@web.de>
2019-08-18 10:46:53 +0200

    Touch screen compatibility.

--- Player.gd
+++ Player.gd
@@ -5,8 +5,8 @@ func _process(delta):
 	if State.in_goal == State.chars:
 		get_node("../UI/GameOver").game_over()
 
-func _unhandled_key_input(event):
-	if Input.is_action_pressed("key_split"):
+func _unhandled_input(event:InputEvent):
+	if event.is_action_pressed("key_split"):
 		if State.splits_rem > 0:
 			State.splits_rem -= 1
 			get_tree().call_group("Chars", "split")
